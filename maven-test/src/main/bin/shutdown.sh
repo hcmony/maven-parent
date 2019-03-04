14553329a25be5eb529439e44cee0cd04e006599
hcmony <houhongchao@bianxianmao.com>
2019-03-04 16:15:44 +0800

    update

--- maven-test/src/main/bin/shutdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pid=`ps aux|grep ${jar.name}|grep -v grep |awk '{print $2}'`
-if [ -n "${pid}" ]; then
-  kill ${pid}
-  echo `ps aux|grep ${jar.name}|grep -v grep`
-else
-  echo "WARNING: ${jar.name} does not started!"
-fi
